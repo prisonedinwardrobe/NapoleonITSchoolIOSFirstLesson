9797947709817a1a7e38eed801f15f85dd25bdc9
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-04-21 13:11:00 +0500

    first commit

---
+++ FirstLesson/ProductViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ProductViewController.swift
+//  FirstLesson
+//
+//  Created by alexey on 4/21/18.
+//  Copyright © 2018 CSU. All rights reserved.
+//
+
+import UIKit
+
+struct Product {
+    let imageName   : String
+    let title       : String
+    let price       : Float
+}
+
+class ProductViewController: UIViewController {
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    
+    var product: Product?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViewController()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadProduct()
+    }
+
+    @IBAction func actionBuy(_ sender: UIButton) {
+        
+    }
+    
+    func setupViewController() {
+        product = Product(imageName: "image-product", title: "iPhone 6S Rose Gold 64GB почти новый все чеки", price: 18500)
+    }
+
+    func loadProduct() {
+        guard let product = product else {
+            print("<ProductViewController: loadProduct> ERROR product is nil!!!")
+            return
+        }
+        
+        mainImageView.image = UIImage(named: product.imageName)
+        titleLabel.text = product.title
+        priceLabel.text = "\(product.price) ₽";
+    }
+    
+}
